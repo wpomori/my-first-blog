37e51de2f39c893dda96a76d72588d85f3ca6f31
Wellington Pine Omori <wpomori@gmail.com>
2018-06-15 04:47:07 -0300

    Changed HTML for the site.

---
+++ blog/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from . import views
+
+# "r'^$'" representa uma URL 'http://127.0.0.1:8000 /', onde entre o '0' e ' /' existe o espaço, pois a expressão regular não reconhece o '/'.
+# Então, tudo o que está depois de 'http://127.0.0.1:8000' é vazio, por isso, quando o padrão identifica o vazio ele executa a 'views' e 'post_list'
+# Então, quando o usuário digita 'http://127.0.0.1:8000 /', Django entende através de views.post_list que está no lugar correto
+urlpatterns = [
+    url(r'^$', views.post_list, name='post_list'),
+]
+
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+# A função '(def)' chamada 'post_list' que recebe um 'request' e retorna '(return)' uma função 'render', que irá renderizar (montar) o modelo 'blog/post_list.html'.
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
+
