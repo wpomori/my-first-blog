7f09d5c12862e14f8e4b02ba4e5000e06f0bb991
Wellington Pine Omori <wpomori@gmail.com>
2018-06-15 15:53:55 -0300

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,6 @@ from . import views
 # Então, quando o usuário digita 'http://127.0.0.1:8000 /', Django entende através de views.post_list que está no lugar correto
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
 ]
 
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
+from django.shortcuts import render, get_object_or_404
+
 
 # A função '(def)' chamada 'post_list' que recebe um 'request' e retorna '(return)' uma função 'render', que irá renderizar (montar) o modelo 'blog/post_list.html'.
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
-
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
